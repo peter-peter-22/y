285a3d5fabfb5a7432f22f6c15f25b21d3fa33d0
Magyar-Peter-22 <81881238+Magyar-Peter-22@users.noreply.github.com>
2024-08-27 01:14:47 +0200

    update loading screen
    create proper graphic for the global loading screen

--- frontend/my-react-app/src/components/loading.jsx
+++ frontend/my-react-app/src/components/loading.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import {  logo } from '/src/components/utilities';
 
-export default ()=>{
-    return(
-        <p>loading</p>
+export default () => {
+    return (
+        <div style={{
+            height: "100vh",
+            width: "100vw",
+            backgroundColor: "black",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center"
+        }}>
+            <img src={logo} style={{
+                height: "80px",
+                filter: "invert(1)"
+            }} />
+        </div>
     );
 }
