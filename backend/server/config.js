d9978e6d91bcc42d53bb92095e0f06c2acd9085b
Magyar-Peter-22 <81881238+Magyar-Peter-22@users.noreply.github.com>
2024-08-30 10:28:39 +0200

    fix env, revert pg log

--- backend/server/config.js
+++ backend/server/config.js
@@ -52,15 +52,13 @@ const transporter = nodemailer.createTransport({
 });
 
 //pg
-const test={
+const pgPool = new pg.Pool({
     user: process.env.PG_USER,
     host: process.env.PG_HOST,
     database: process.env.PG_DATABASE,
     password: process.env.PG_PASSWORD,
     port: process.env.PG_PORT,
-};
-console.log(test);
-const pgPool = new pg.Pool(test);
+});
 
 async function initialize() {
     const db = pgPool;
--- frontend/my-react-app/src/components/config.js
+++ frontend/my-react-app/src/components/config.js
@@ -1,15 +1,14 @@
-import env from "dotenv";
-env.config();
-
 const accepted_image_types = ["image/png", "image/jpeg", "image/jpg"];
 const accepted_video_types = ["video/mp4"];
+
+const env=import.meta.env;
 
 const config = {
     accepted_image_types: accepted_image_types.join(", "),
     accepted_media_types: [...accepted_image_types, ...accepted_video_types].join(", "),
     address_mode: {
-        server: process.env.REACT_APP_SERVER_URL,
-        client: process.env.REACT_APP_CLIENT_URL,
+        server: env.VITE_SERVER_URL,
+        client: env.VITE_CLIENT_URL,
     },
 }
 
