89195c37664fb1d46d06b43311634f3e4fefb57d
Magyar-Peter-22 <81881238+Magyar-Peter-22@users.noreply.github.com>
2024-09-04 10:14:38 +0200

    log session maxage

--- backend/server/components/app_use.js
+++ backend/server/components/app_use.js
@@ -36,7 +36,7 @@ function initialize() {
             cookie: {
                 secure: https, // Set to true if using HTTPS
                 sameSite: https ? "none" : "lax",
-                maxAge: config.false,
+                maxAge: false,
                 httpOnly: false,
             }
         })
@@ -45,6 +45,7 @@ function initialize() {
     //update session expiration 
     app.use((req, res, next) => {
         req.session.lastVisit = new Date();
+        console.log(req.session.cookie.maxAge);
         next();
     });
 
