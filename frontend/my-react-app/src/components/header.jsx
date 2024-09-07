b75ce9508fc7904aaa190b58e6fad23086b8cea2
Magyar-Peter-22 <81881238+Magyar-Peter-22@users.noreply.github.com>
2024-09-07 16:23:07 +0200

    a

--- frontend/my-react-app/src/components/header.jsx
+++ frontend/my-react-app/src/components/header.jsx
@@ -292,8 +292,8 @@ function LeftTabs({ lastTab, bottom, tabs }) {
         console.log(alwaysHiddenTabs);
     }
     return (
-        <div style={{ width: width, height: "100%", flexShrink: 0,overflow:"hidden" }}>
-            <div style={{ position: "fixed", width: width, height: "100vh",overflow:"hidden" }}>
+        <div style={{ width: width, height: "100%", flexShrink: 0 }}>
+            <div style={{ position: "fixed", width: width, height: "100%",overflow:"hidden" }}>
                 <Inside>
                     <div style={{ display: "flex", flexDirection: "column", justifyContent: "space-between", alignItems: wideButtons ? "stretch" : "center", height: "100%",overflow:"hidden" }}>
                         {/*this div show the maximum available space for the tabs*/}
