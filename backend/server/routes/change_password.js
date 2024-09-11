521dde441f8ad8417e1ce399d9a60850bc79b170
Magyar-Peter-22 <81881238+Magyar-Peter-22@users.noreply.github.com>
2024-09-12 01:25:17 +0200

    fix password recovery

--- backend/server/routes/change_password.js
+++ backend/server/routes/change_password.js
@@ -19,13 +19,16 @@ router.post("/submit_chapta", async (req, res) => {
     const { email, recaptchaToken } = req.body;
 
     //get the user that belong to this email
-    const user_query = await db.query("select id from users where email=$1", [email.toLowerCase()]);
+    const user_query = await db.query("select id,email,password_hash from users where email=$1", [email.toLowerCase()]);
     if (user_query.rowCount === 0)
         CheckErr("No Y user belongs to this email");
-    const user_id = user_query.rows[0].id;
+    const user = user_query.rows[0];
+    const user_id = user.id;
+    if (user.password_hash)
+        CheckErr("This email was registered with Google authentication. It has no password.");
 
     //check rechapta solution
-    if (!skip) await CheckRechapta(recaptchaToken,req);
+    if (!skip) await CheckRechapta(recaptchaToken, req);
 
     //generate secret code
     const code_query = await (db.query(create_secret, [user_id]));
@@ -35,7 +38,7 @@ router.post("/submit_chapta", async (req, res) => {
     const link = config.address_mode.client + "/change_password/" + user_id + "/" + secret;
     const mailOptions = {
         from: `Y Password change <${g.transporter.options.auth.user}>`,
-        to: req.body.email,
+        to: email,
         subject: 'Password change',
         text: `
 Open the link below to change your password.
