22f0a39d150e95381b38974bdc95beb5dbcbfab6
Magyar-Peter-22 <81881238+Magyar-Peter-22@users.noreply.github.com>
2024-09-12 00:46:04 +0200

    remove testing value

--- backend/server/routes/user.js
+++ backend/server/routes/user.js
@@ -49,7 +49,7 @@ router.get("/get", async (req, res) => {
     //console.log(`\n\nget user: \nuser: ${JSON.stringify(req.user)} \nsession: ${JSON.stringify(req.session)}\n\n` );
     res.json({
         user: req.user,
-        showStartMessage: req.session.showStartMessage||true,
+        showStartMessage: req.session.showStartMessage,
         pending_registration: req.session.pending_registration,
         maxLetters:GetMaxLetters(req.user)
     });
